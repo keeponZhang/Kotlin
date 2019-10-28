627dd838a60f78f5b053a1893f9430df4187e3cf
zhangwengao <zhangwengao@yy.com>
2019-10-28 18:34:10 +0800

    no message

--- app/src/main/java/com/keepon/kotlin/chapter12/2.2.1 将类型形参替换为具体类型.kt
+++ app/src/main/java/com/keepon/kotlin/chapter12/2.2.1 将类型形参替换为具体类型.kt
@@ -3,3 +3,58 @@ package com.keepon.kotlin.chapter12
 /**
  * createBy	 keepon
  */
+//下面我们先定义一个泛型类Holder<T>，再将它的类型形参替换为具体类型Int：
+
+interface Holder<T> {
+    fun getValue() : T
+    fun setValue(t : T)
+}
+
+class HolderInt : Holder<Int> {
+    var a : Int = 0;
+    override fun getValue() = a
+    override fun setValue(value : Int) {
+        a = value
+    }
+}
+
+fun main(args: Array<String>) {
+    val t = HolderInt()
+    t.setValue(2)
+    println("value=${t.getValue()}")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
