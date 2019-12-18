3c7e38a3b3c490099d22fa4f35f9a90520962482
zhangwengao <zhangwengao@yy.com>
2019-12-18 18:31:27 +0800

    no message

--- app/src/main/java/com/kotlin/action/ch04/4.2.1_InitializingClasses.kt
+++ app/src/main/java/com/kotlin/action/ch04/4.2.1_InitializingClasses.kt
@@ -1,8 +1,23 @@
 package ch04.ex2_1_InitializingClasses
 
 //这段被括号围起来的语句块就叫作主构造方法
-class User(val nickname: String,
-           val isSubscribed: Boolean = true)
+class User(
+    val nickname: String,  //。如果属性用相应的构造方法参数来初始化
+    val isSubscribed: Boolean = true
+)
+
+//如果主构造方法没有注解或可见性修饰符，同样可以去掉constructor 关键字。
+class User2 constructor(
+    _nickname: String,   //构造方法参数
+    val isSubscribed: Boolean = true
+) {
+    //属性
+    val nickname: String
+
+    init {
+        nickname = _nickname
+    }
+}
 
 fun main(args: Array<String>) {
     val alice = User("Alice")
