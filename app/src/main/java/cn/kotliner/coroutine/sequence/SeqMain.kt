276b36a6163f173ca3eadbce129e57b5de4cb5c2
zhangwengao <zhangwengao@yy.com>
2021-06-13 23:56:12 +0800

    feat:增加对sequence的理解

--- app/src/main/java/cn/kotliner/coroutine/sequence/SeqMain.kt
+++ app/src/main/java/cn/kotliner/coroutine/sequence/SeqMain.kt
@@ -1,9 +1,7 @@
 package cn.kotliner.coroutine.sequence
 
 import cn.kotliner.coroutine.common.log
-import kotlin.coroutines.experimental.SequenceBuilder
 import kotlin.coroutines.experimental.buildSequence
-import kotlin.coroutines.experimental.intrinsics.createCoroutineUnchecked
 
 /**
  * Created by benny on 5/29/17.
@@ -16,11 +14,12 @@ fun main(args: Array<String>) {
 
     val var2: Iterator<Int> = fibonacci.iterator()
     //里面是个死循环，如果没有返回值，会一直调用resume，从而走到下面的suspend方法，改变state，执行挂起操作,执行到yield后，会改变状态，var2.hasNext()
-    // 返回true
+    // 返回true（yield启动的时候就会首先调用）
+    //这里的逻辑是，调用var2.hasNext()返回true，下面的yield阻塞住的才会被恢复，跳出循环的话，下面的也不会被恢复
     while (var2.hasNext()) {
         val i: Int = (var2.next() as Number).toInt()
         val var3 = false
-        println(i)
+        println("上面循环 $i")
         if (i > 100) {
             break
         }
@@ -38,16 +37,18 @@ fun main(args: Array<String>) {
 //    return iterator
 //iterator.nextStep被赋值Coroutine
 //}
-val fibonacci = buildSequence() {
+//buildSequence的函数参数是SequenceBuilder类型的，返回Sequence<T>,<T>是yield的参数类型
+val fibonacci: Sequence<Int> = buildSequence() {
 
 //    yield(1) //每次调用，把值赋值给nextValue,然后挂起
     var cur = 1
     var next = 1
-    println("-------只会调用一次-----")
+    println("-------下面初始化只会调用一次-----")
 //    我们只需要在while循环体中循环调用yield()来检查该job的取消状态，如果已经被取消，那么isCompleted值将会是true，yield函数就直接抛出CancellationException异常，从而完成取消的功能：
     while (true) {
+        //后面需要Continuation.resume回调
         try {
-            //yield，会阻塞住，调用者调用var2.hasNext()
+            //yield，会阻塞住，调用者调用var2.hasNext()，才会接着执行下面的代码
             yield(next) //类似 我要开始耗时操作了方法
         } catch (e: Exception) {
             log("Exception")
@@ -57,7 +58,6 @@ val fibonacci = buildSequence() {
         cur = next
         next = tmp
     }
-
 
 
 }
