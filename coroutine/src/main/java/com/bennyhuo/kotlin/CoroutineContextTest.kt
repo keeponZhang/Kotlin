c494aedcd44e58b779ce3b093614c1e38d0fd194
zhangwengao <zhangwengao@joyy.sg>
2021-06-25 18:49:23 +0800

    no message

--- coroutine/src/main/java/com/bennyhuo/kotlin/CoroutineContextTest.kt
+++ coroutine/src/main/java/com/bennyhuo/kotlin/CoroutineContextTest.kt
@@ -11,9 +11,15 @@ import kotlin.coroutines.EmptyCoroutineContext
  *createBy keepon
  */
 fun main() {
+//    val asyncContext = AsyncContext("chenmo")
+//    val asyncContext2 = AsyncContext("chenmo2")
+//    val asyncContext3 = AsyncContext("chenmo3")
+//    val coroutineContext = asyncContext + asyncContext2
+
+
     val asyncContext = AsyncContext("chenmo")
-    val asyncContext2 = AsyncContext("chenmo2")
-    val asyncContext3 = AsyncContext("chenmo3")
+    val asyncContext2 = AsyncContext2("chenmo2")
+    val asyncContext3 = AsyncContext3("chenmo3")
     val coroutineContext = asyncContext + asyncContext2
     coroutineContext + asyncContext3
     println("-----------")
@@ -26,6 +32,22 @@ class AsyncContext(val test: String = "keepon") : AbstractCoroutineContextElemen
 //    public override operator fun <E : CoroutineContext.Element> get(key: CoroutineContext.Key<E>): E? =
 //        if (this.key === key) this as E else null
     companion object Key : CoroutineContext.Key<AsyncContext>
+}
+class AsyncContext2(val test: String = "keepon2") : AbstractCoroutineContextElement(Key) {
+
+    //AbstractCoroutineContextElement需要一个key，类型是CoroutineContext.Key，最好是单例，
+    //好难搞，这样处理就是为了拿到AsyncContext2的实例，通过对比key，相等的话直接转换，否则返回null
+//    public override operator fun <E : CoroutineContext.Element> get(key: CoroutineContext.Key<E>): E? =
+//        if (this.key === key) this as E else null
+    companion object Key : CoroutineContext.Key<AsyncContext2>
+}
+class AsyncContext3(val test: String = "keepon3") : AbstractCoroutineContextElement(Key) {
+
+    //AbstractCoroutineContextElement需要一个key，类型是CoroutineContext.Key，最好是单例，
+    //好难搞，这样处理就是为了拿到AsyncContext2的实例，通过对比key，相等的话直接转换，否则返回null
+//    public override operator fun <E : CoroutineContext.Element> get(key: CoroutineContext.Key<E>): E? =
+//        if (this.key === key) this as E else null
+    companion object Key : CoroutineContext.Key<AsyncContext3>
 }
 
 class DownloadContext(val url: String) : AbstractCoroutineContextElement(DownloadContext) {
